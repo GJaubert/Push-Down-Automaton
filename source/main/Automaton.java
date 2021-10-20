905fc5df4e1c16763eae0e39a574d729b8f1c323
gabriel garcia jaubert <alu0101240374@ull.edu.es>
2021-10-20 23:55:03 +0100

    Funcionan algunos ejemplos con Apv-2

--- source/main/Automaton.java
+++ source/main/Automaton.java
@@ -50,6 +50,7 @@ public class Automaton {
     initialStackSymbol = new Symbol(inputInitialStackSymbol);
     //System.out.println(initialStackSymbol.value);
     stack.push(initialStackSymbol);
+    System.out.println(stack.size());
     stackElements.addFirst(stack.peek());
     
     transitions = new Delta(inputTransitions);
@@ -79,24 +80,33 @@ public class Automaton {
   }
 
   public void transit(String inputString, State currentState) {
-    if (stack.empty()) {
-      if (inputString.length() == 0)
+    if (inputString.length() == 0) {
+      if (stack.empty()) {
         stringAccepted = true;
+        System.out.println("Aceptada");
+      }
+    }
+    if (stack.empty()) {
       return;
     }
+    //System.out.println(stack.peek().value);
     Vector<Map.Entry<State, Vector<Symbol>>> possibleTransitions = findTransitions(inputString, currentState);
     if (possibleTransitions.size() == 0) {
+      System.out.println("No quedan mas");
       return;
     }
     //aqui debo hacer una foto de la pila;
-    Stack<Symbol> stackSnapShot = stack;
+    Stack<Symbol> stackSnapShot = new Stack<Symbol>();
+    stackSnapShot.addAll(stack);
     LinkedList<Symbol> stackElementsCopy = stackElements;
     //System.out.println(possibleTransitions);
     for (int i = 0; i < possibleTransitions.size(); i++) {
-     if (i > 0) stack = stackSnapShot;
-     printTransition(inputString, possibleTransitions.get(i), currentState);
-     executeTransition(inputString, possibleTransitions.get(i));
-     if (stringAccepted == true) break;
+      stack.removeAllElements();
+      stack.addAll(stackSnapShot);
+      //System.out.println(possibleTransitions.size());
+      printTransition(inputString, possibleTransitions.get(i), currentState, stackSnapShot);
+      executeTransition(inputString, possibleTransitions.get(i));
+      if (stringAccepted == true) break;
     }
   }
 
@@ -104,25 +114,30 @@ public class Automaton {
   public Vector<Map.Entry<State, Vector<Symbol>>> findTransitions(String inputString, State currentState) {
     Vector<Map.Entry<State, Vector<Symbol>>> possibleTransitions = new Vector<Map.Entry<State, Vector<Symbol>>>();
     Vector<Symbol> inputSymbols = new Vector<Symbol>();
-    inputSymbols.add(new Symbol(String.valueOf(inputString.charAt(0))));    //Primer caracter de la cadena
+    inputSymbols.add(new Symbol("."));
     inputSymbols.add(stack.peek());   //Primer elemento de la pila
-    Map.Entry<State, Vector<Symbol>> deltaMapKey = new AbstractMap.SimpleEntry<State, Vector<Symbol>>(currentState, inputSymbols);
-    Set<Map.Entry<State, Vector<Symbol>>> destinationsSet = transitions.getTransitionsMap().get(deltaMapKey);
-    inputSymbols.set(0, new Symbol("."));
     Map.Entry<State, Vector<Symbol>> epsilonMapKey = new AbstractMap.SimpleEntry<State, Vector<Symbol>>(currentState, inputSymbols);
     Set<Map.Entry<State, Vector<Symbol>>> epsilonSet = transitions.getTransitionsMap().get(epsilonMapKey);
-    if (destinationsSet != null) {
-      Iterator<Map.Entry<State, Vector<Symbol>>> itr = destinationsSet.iterator();
-      while (itr.hasNext()) {
-        possibleTransitions.add(itr.next());
-      }
-    }
     if (epsilonSet != null) {
       Iterator<Map.Entry<State, Vector<Symbol>>> itr = epsilonSet.iterator();
       while (itr.hasNext()) {
         Map.Entry<State, Vector<Symbol>> tmp = itr.next();
-        tmp.getValue().add(new Symbol("&"));    //Caracter reservado para saber si es una transicion que no consume caracter
+        if (!tmp.getValue().get(tmp.getValue().size() - 1).value.equals("&")) {
+          tmp.getValue().add(new Symbol("&"));    //Caracter reservado para saber si es una transicion que no consume caracter
+        }
         possibleTransitions.add(tmp);
+      }
+    }
+    if (inputString.length() > 0) {
+      inputSymbols.set(0, new Symbol(String.valueOf(inputString.charAt(0))));    //Primer caracter de la cadena
+      //System.out.println("probando con " + String.valueOf(inputString.charAt(0)) + " " + stack.peek().value);
+      Map.Entry<State, Vector<Symbol>> deltaMapKey = new AbstractMap.SimpleEntry<State, Vector<Symbol>>(currentState, inputSymbols);
+      Set<Map.Entry<State, Vector<Symbol>>> destinationsSet = transitions.getTransitionsMap().get(deltaMapKey);
+      if (destinationsSet != null) {
+        Iterator<Map.Entry<State, Vector<Symbol>>> itr = destinationsSet.iterator();
+        while (itr.hasNext()) {
+          possibleTransitions.add(itr.next());
+        }
       }
     }
     return possibleTransitions;
@@ -132,29 +147,40 @@ public class Automaton {
     //falta manejar la pila
     String newString = inputString;
     Vector<Symbol> stackSymbols = transition.getValue();
-    if (stackSymbols.get(stackSymbols.size() - 1).value != "&") {
-      newString = newString.substring(1);
+    if (stackSymbols.get(stackSymbols.size() - 1).value.equals("&")) {
+      //System.out.println("borra");
+      //stackSymbols.remove(stackSymbols.size() - 1);
     } else {
-      stackSymbols.remove(stackSymbols.size() - 1);
+      newString = newString.substring(1);
     }
     stack.pop();
-    stackElements.remove();
+    //stackElements.remove();
     for (int i = stackSymbols.size() - 1; i >= 0; i--) {
-      if (stackSymbols.get(i).value.equals(".")) continue;
+      if (stackSymbols.get(i).value.equals(".") || stackSymbols.get(i).value.equals("&")) continue;
       stack.push(stackSymbols.get(i));
+      //System.out.println(stack.peek().value);
+      //System.out.println("stack symbol: " + stackSymbols.get(i).value);
       stackElements.addFirst(stack.peek());
       //System.out.println(stackSymbols.size());
     }
     transit(newString, transition.getKey());
   }
 
-  public void printTransition(String inputString, Map.Entry<State, Vector<Symbol>> transition, State currentState) {
-    if (transition.getValue().get(transition.getValue().size() - 1).value == "&") currentState = new State(".", false);
-    System.out.print(currentState.getName() + " " + inputString + " ");
-    for (int i = 0; i < stackElements.size(); i++) {
-      System.out.print(stackElements.get(i).value);
+  public void printTransition(String inputString, Map.Entry<State, Vector<Symbol>> transition, State currentState, Stack<Symbol> stackSnapShot) {
+    String consumedSymbol;
+    if (inputString.length() > 0 && transition.getValue().get(transition.getValue().size() - 1).value != "&") {
+      consumedSymbol = String.valueOf(inputString.charAt(0));
+    } else {
+      consumedSymbol = ".";
     }
-    System.out.print(" " + transition.getKey().getName() + " ");
+    if (transition.getValue().get(transition.getValue().size() - 1).value == "&") consumedSymbol = ".";
+    System.out.print(currentState.getName() + " " + consumedSymbol + " " + inputString + "\t");
+    if (inputString.length() <= 3) System.out.print("\t");
+    System.out.print(stack.peek().value);
+    // for (int i = 0; i < stackElements.size(); i++) {
+    //   System.out.print(stackElementsCopy.get(i).value);
+    // }
+    System.out.print("\t" + transition.getKey().getName() + " ");
     for (int i = 0; i < transition.getValue().size(); i++) {
       if (transition.getValue().get(i).value.equals("&")) continue;
       System.out.print(transition.getValue().get(i).value);
--- source/main/AutomatonProject.java
+++ source/main/AutomatonProject.java
@@ -9,6 +9,6 @@ public class AutomatonProject {
     Automaton automaton = new Automaton(reader.getStates(), reader.getAlphabet(), 
                                         reader.getStackAlphabet(), reader.getinitialState(),
                                         reader.getStartingStackSymbol(), reader.getTransitions());
-    automaton.checkString("0110");
+    automaton.checkString("11");
   }
 }
